c5e817ae5d0414d64e9e9afdc32a4aab9b55baf8
Chris Wong <email": null>
2016-06-30 18:49:39 -0700

    Add google map

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 });
 
-function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 8
-  });
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+// https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingStatusCodes
