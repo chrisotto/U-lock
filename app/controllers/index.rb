9a44b04296fbf4f04936c27ce7617d3f55d0e4ca
Chris Otto <email": null>
2016-07-01 12:32:47 -0700

    Add stolen bike listings and gentle css stylings

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,4 @@
 get '/' do
-	
+	@bikes = JSON.parse(Net::HTTP.get(URI('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=94107&proximity_square=100')))["bikes"]
 	erb :'index'
 end
