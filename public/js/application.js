b02efad227e34de0c9b8c5b9a44ce729709fae4b
Chris Otto <email": null>
2016-07-01 12:05:27 -0700

    Fix map

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
 });
 
+function drawMap() {
+
+}
+
+// https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingStatusCodes
+
 var geocoder;
 var map;
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
+function initMap() {
+  geocoder = new google.maps.Geocoder;
+  var latlng = new google.maps.LatLng(37.7749, -122.4194);
   var mapOptions = {
-    zoom: 8,
+    zoom: 11,
     center: latlng
   }
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
-// https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingStatusCodes
+function codeAddress() {
+  var address = document.getElementById("address").value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
